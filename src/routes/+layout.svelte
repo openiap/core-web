d74c38fab54cb186c447e001fbed902a54463eee
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2024-12-03 11:28:00 +0300

    Added sidebar fixed header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,55 +4,95 @@
 
 	import { auth } from "$lib/stores/auth.svelte";
 	let { children, data } = $props();
-	
+
 	auth.config = data.config;
 	auth.origin = data.origin;
 	auth.baseurl = data.baseurl;
 	auth.wsurl = data.wsurl;
-	auth.clientinit()
+	auth.clientinit();
 
 	import { Button } from "$lib/components/ui/button/index.js";
 	import { ModeWatcher } from "mode-watcher";
-	
 
 	import { base } from "$app/paths";
 	import { page } from "$app/stores";
 	import { pushState } from "$app/navigation";
 
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+	// import * as Breadcrumb from "$lib/components/ui/breadcrumb/index.js";
+	// import { Separator } from "$lib/components/ui/separator/index.js";
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+
+	// import SidebarPage from "$lib/components/sidebar-page.svelte";
+
 	if ($page.url.search.includes("code=")) {
 		console.debug("Code", $page.url.searchParams.get("code"));
 		auth.userManager
 			.signinRedirectCallback()
-			.then(async function (user:any) {
+			.then(async function (user: any) {
 				pushState(base + "/", {});
 				await auth.loadUserAndClient();
 			})
-			.catch((error:any) => {
+			.catch((error: any) => {
 				console.debug(error);
 			});
 	}
-	let pagename = $derived(()=> $page.url.pathname.replace(base, "").replace("/", ""));
+	let pagename = $derived(() =>
+		$page.url.pathname.replace(base, "").replace("/", ""),
+	);
+
 </script>
+
 <svelte:head>
-  <title>OpenCore {pagename()}</title>
-  <meta
-    name="description"
-    content="Get performance data and generates index suggestions for better database performance"
-  />
+	<title>OpenCore {pagename()}</title>
+	<meta
+		name="description"
+		content="Get performance data and generates index suggestions for better database performance"
+	/>
 </svelte:head>
 <ModeWatcher />
 {#if $page.url.pathname != base + "/login" && $page.url.pathname != base + "/loginscreen"}
 	<div class="app">
-		<Header />
+		<Sidebar.Provider>
+			<AppSidebar />
+			<Sidebar.Inset>
+				<header
+					class="flex h-16 shrink-0 items-center justify-between gap-2 border-b px-4"
+				>
+					<div class="flex items-center">
+						<Sidebar.Trigger class="-ml-1" />
+						<!-- <Separator orientation="vertical" class="mr-2 h-4" />
+						<Breadcrumb.Root>
+							<Breadcrumb.List>
+								<Breadcrumb.Item class="hidden md:block">
+									<Breadcrumb.Link href="#"
+										>Building Your Application</Breadcrumb.Link
+									>
+								</Breadcrumb.Item>
+								<Breadcrumb.Separator class="hidden md:block" />
+								<Breadcrumb.Item>
+									<Breadcrumb.Page
+										>Data Fetching</Breadcrumb.Page
+									>
+								</Breadcrumb.Item>
+							</Breadcrumb.List>
+						</Breadcrumb.Root> -->
+					</div>
+					<Header openHelp={open} />
+				</header>
 
-		<main>
-			{#if auth.isAuthenticated == true}
-				<!-- <p>Logged in</p> -->
-				{@render children()}
-			{:else}
-				<!-- <p>Not logged in</p> -->
-			{/if}
-		</main>
+				<div class="flex flex-1 flex-col gap-4 p-4">
+					<main>
+						{#if auth.isAuthenticated == true}
+							<!-- <p>Logged in</p> -->
+							{@render children()}
+						{:else}
+							<!-- <p>Not logged in</p> -->
+						{/if}
+					</main>
+				</div>
+			</Sidebar.Inset>
+		</Sidebar.Provider>
 
 		<!-- <footer>
 			<p>
@@ -87,7 +127,7 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
-/* 
+	/* 
 	footer {
 		display: flex;
 		flex-direction: column;
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import logo from "$lib/images/logo.png";
-	import github from "$lib/images/github.svg";
 	import Sun from "lucide-svelte/icons/sun";
 	import Moon from "lucide-svelte/icons/moon";
 	import { toggleMode } from "mode-watcher";
 	import { HotkeyButton } from "$lib/components/ui/hotkeybutton/index.js";
 	import { base } from "$app/paths";
 	import { auth } from "$lib/stores/auth.svelte";
+	import { Github, HelpCircle } from "lucide-svelte";
+	import { Button } from "$lib/components/ui/button";
+
+	let props = $props();
+
 	function login() {
 		auth.userManager.signinRedirect();
 	}
@@ -17,66 +21,34 @@
 </script>
 
 <header>
-	<div class="corner">
+	<!-- <div class="corner">
 		<a href="/">
 			<img src={logo} alt="Logo" />
 		</a>
-	</div>
+	</div> -->
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li
-				aria-current={$page.url.pathname === base + "/"
-					? "page"
-					: undefined}
-			>
-				<a href="{base}/">Home</a>
-			</li>
-			{#if auth.isAuthenticated == true}
-				<li
-					aria-current={$page.url.pathname === base + "/user"
-						? "page"
-						: undefined}
-				>
-					<a href="{base}/user">Users</a>
-				</li>
-				<li
-					aria-current={$page.url.pathname.startsWith(base + "/role")
-						? "page"
-						: undefined}
-				>
-					<a href="{base}/role">Roles</a>
-				</li>
-				<li
-					aria-current={$page.url.pathname.startsWith(
-						base + "/entities",
-					)
-						? "page"
-						: undefined}
-				>
-					<a href="{base}/entities">Entities</a>
-				</li>
-			{/if}
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner flex justify-end">
+	<div class="flex justify-end items-center justify-center">
 		<div class="flex gap-1.5">
+			<!-- <Button
+				variant="outline"
+				onclick={() => {
+					console.log("test");
+					props.openHelp = true;
+				}}
+			>
+				<HelpCircle />
+			</Button> -->
 			<a href="https://github.com/openiap">
-				<img src={github} alt="GitHub" />
+				<Button variant="outline">
+					<Github width="100%" />
+				</Button>
 			</a>
 			<HotkeyButton onclick={toggleMode} variant="outline" size="icon">
 				<Sun
-					class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
 				/>
 				<Moon
-					class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
 				/>
 				<span class="sr-only">Toggle theme</span>
 			</HotkeyButton>
@@ -98,84 +70,6 @@
 	header {
 		display: flex;
 		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current="page"]::before {
-		--size: 6px;
-		content: "";
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
 	}
 
 	a:hover {
