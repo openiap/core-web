4d0f946de91e4e84240810b86674672333725572
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2024-12-10 14:49:02 +0300

    Fixed mail history page

---
+++ src/routes/mailhistory/+page.server.ts
@@ -0,0 +1,15 @@
+import { auth } from "$lib/stores/auth.svelte.js";
+import type { PageServerLoad } from "./$types.js";
+export const load: PageServerLoad = async ({ fetch, url, cookies, locals }) => {
+    await auth.clientinit((locals as any).domain, url.origin, fetch, cookies);
+    const entities = await auth.client.Query({
+        collectionname: "mailhist",
+        query: {},
+        orderby: "",
+        skip: 0,
+        top: 5,
+        jwt: auth.access_token,
+    })
+    return { entities };
+
+};
