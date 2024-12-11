e44eb214b8c9bc4aeeb4e21bf3cf6748b7e3d91e
skadefro <allan@openrpa.dk>
2024-12-12 00:52:26 +0100

    test, remove cookie path

--- src/lib/stores/SvelteStorage.ts
+++ src/lib/stores/SvelteStorage.ts
@@ -2,11 +2,11 @@ import { CookieStorage } from '../cookie-storage';
 
 export class SvelteStorage {
     private cookies: any;
-    private storage = new CookieStorage();
+    private storage = null as any;
     constructor(cookies: any) {
         this.cookies = cookies;
         if (this.cookies == null) {
-            this.storage = new CookieStorage();
+            this.storage = new CookieStorage(); // {path: "/"}
         }
     }
     clear() {
@@ -28,12 +28,12 @@ export class SvelteStorage {
         let provider = "cookiestorage";
         if (this.cookies == null) provider = "cookie";
         let urlencodedkey = encodeURIComponent(key);
-        this.cookies ? this.cookies.set(urlencodedkey, value) : this.storage.setItem(key, value, {path: "/"});
+        this.cookies ? this.cookies.set(urlencodedkey, value) : this.storage.setItem(key, value); // , {path: "/"}
     }
     removeItem(key: string): void {
         let urlencodedkey = encodeURIComponent(key);
         let provider = "cookiestorage";
         if (this.cookies == null) provider = "cookie";
-        this.cookies ? this.cookies.delete(urlencodedkey) : this.storage.removeItem(key, {path: "/"});
+        this.cookies ? this.cookies.delete(urlencodedkey) : this.storage.removeItem(key); // , {path: "/"}
     }
 }
