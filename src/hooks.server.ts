200a018ff80160701ddfc01bac96c70f254ad482
skadefro <allan@openrpa.dk>
2024-12-12 17:29:03 +0100

    clean

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,11 @@
 import type { Handle } from '@sveltejs/kit';
 import { domain, client_id } from '$env/static/private';
 import { auth } from '$lib/stores/auth.svelte';
-
 export const handle: Handle = async ({ event, resolve }) => {
     const { url, cookies } = event;
     await auth.clientinit(domain, client_id, url.origin, fetch, cookies );
     event.locals = event.locals || {};
-
     console. debug("hooks.server.ts: ", "conn", auth.isConnected, "auth", auth.isAuthenticated, auth.profile?.name);
-
     // @ts-ignore
     event.locals.domain = domain;
     // @ts-ignore
