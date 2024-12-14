f88521f2e3a6f535b9db99ec627a5d24dc04cbc2
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2024-12-14 19:28:36 +0300

    deleted page.ts

--- src/routes/customer/+page.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from "$lib/stores/auth.svelte.js";
-import { base } from "$app/paths";
-import type { PageLoad } from "./$types.js";
-export const load: PageLoad = async (x: any) => {
-    const { data, fetch, cookies, url } = x;
-
-    return data;
-
-};
