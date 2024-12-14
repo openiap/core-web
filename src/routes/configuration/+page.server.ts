6f1306693b9e888c91e50f9364d75761d7fd0b0d
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2024-12-14 19:27:13 +0300

    added config page

--- src/routes/configuration/+page.server.ts
+++ src/routes/configuration/+page.server.ts
@@ -6,20 +6,20 @@ import { auth } from "$lib/stores/auth.svelte.js";
 import { base } from "$app/paths";
 import { editFormSchema } from "./schema.js";
 import { cleanMatchingKeys, defaultSettings } from "./helper.js";
-
 // const key = "user"
 
 export const load: PageServerLoad = async ({ fetch, url, cookies, locals, params }) => {
   let data: any = {};
   let currentSettings = await auth.client.FindOne<any>({ collectionname: "config", query: { "_type": "config" }, jwt: auth.access_token });
   let combinedSettings = { ...defaultSettings, ...currentSettings }
-
   data.form = await superValidate(combinedSettings, zod(editFormSchema));
+  console.log("data.form");
   return data;
 };
 
 export const actions: Actions = {
   default: async (event: any) => {
+    console.error("actions: ");
     const form = await superValidate(event, zod(editFormSchema));
     if (!form.valid) {
       return fail(400, {
--- src/routes/configuration/+page.svelte
+++ src/routes/configuration/+page.svelte
@@ -10,16 +10,19 @@
   import SuperDebug, { superForm } from "sveltekit-superforms";
   import { zod } from "sveltekit-superforms/adapters";
   import { editFormSchema } from "./schema.js";
+  import { settings } from "./helper.js";
+  import { ObjectInput } from "$lib/objectinput/index.js";
+  import Button from "$lib/components/ui/button/button.svelte";
 
   const key = "user";
   let showdebug = $state(false);
+  let screen = $state("set");
   const { data } = $props();
   const form = superForm(data.form, {
     dataType: "json",
     validators: zod(editFormSchema),
   });
   const { form: formData, enhance, message } = form;
-
 </script>
 
 {#if message && $message != ""}
@@ -29,45 +32,164 @@
 <div>
   Edit {key}
 </div>
-
+<div class="flex items-center space-x-2 my-2">
+  <Button
+    onclick={() => (screen = "all")}
+    variant={screen === "all" ? "default" : "outline"}
+  >
+    All
+  </Button>
+  <Button
+    onclick={() => (screen = "set")}
+    variant={screen === "set" ? "default" : "outline"}
+  >
+    Set
+  </Button>
+  <Button
+    onclick={() => (screen = "unset")}
+    variant={screen === "unset" ? "default" : "outline"}
+  >
+    Unset
+  </Button>
+</div>
 <form method="POST" use:enhance>
   <Form.Button aria-label="submit">Submit</Form.Button>
   <HotkeyButton aria-label="back" onclick={() => goto(base + `/${key}`)}
     >Back</HotkeyButton
   >
 
-  <Acl bind:value={$formData} />
-
-  <!-- <Form.Field {form} name="name">
-    <Form.Control>
-      {#snippet children({ props })}
-        <Form.Label>Name</Form.Label>
-        <Input {...props} bind:value={$formData.name} />
-      {/snippet}
-    </Form.Control>
-    <Form.Description>This is your public display name.</Form.Description>
-    <Form.FieldErrors />
-  </Form.Field> -->
-
-  <Form.Field
-    {form}
-    name="enable_openapi"
-    class="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4"
-  >
-    <Form.Control>
-      {#snippet children({ props })}
-        {#if $formData.enable_openapi != null}
-          <Checkbox {...props} bind:checked={$formData.enable_openapi} />
-          <div class="space-y-1 leading-none">
-            <Form.Label>enable_openapi</Form.Label>
-            <Form.Description>
-              enable_openapi
-            </Form.Description>
-          </div>
-        {/if}
-      {/snippet}
-    </Form.Control>
-  </Form.Field>
+  {#each settings as setting}
+    <!-- {#if setting.type === "boolean"}
+      <div
+        class={`${screen !== "all" ? (screen === "unset" && $formData[setting.name] !== setting.default && "hidden") || (screen === "set" && $formData[setting.name] === setting.default && "hidden") : "block"}`}
+      >
+        <Form.Field
+          {form}
+          name={`${setting.name}`}
+          class="flex flex-row items-center space-x-3 space-y-1 rounded-md border p-4 {$formData[
+            setting.name
+          ] === setting.default && 'opacity-50'}"
+        >
+          <Form.Control>
+            {#snippet children({ props })}
+              <Checkbox {...props} bind:checked={$formData[setting.name]} />
+              <Form.Label
+                >{setting.name}
+                <span class="text-gray-500">(Default: {setting.default})</span>
+              </Form.Label>
+            {/snippet}
+          </Form.Control>
+        </Form.Field>
+      </div>
+    {/if}
+    {#if setting.type === "number"}
+      <div
+        class={`${screen !== "all" ? (screen === "unset" && $formData[setting.name] !== setting.default && "hidden") || (screen === "set" && $formData[setting.name] === setting.default && "hidden") : "block"}`}
+      >
+        <Form.Field
+          {form}
+          name={`${setting.name}`}
+          class="my-2 {$formData[setting.name] === setting.default &&
+            'opacity-50'}"
+        >
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label
+                >{setting.name}
+                <span class="text-gray-500">(Default: {setting.default})</span>
+              </Form.Label>
+              <div class="ms-2 space-y-2">
+                <Input
+                  {...props}
+                  bind:value={$formData[setting.name]}
+                  type="number"
+                />
+              </div>
+            {/snippet}
+          </Form.Control>
+        </Form.Field>
+      </div>
+    {/if}
+    {#if setting.type === "string"}
+      <div
+        class={`${screen !== "all" ? (screen === "unset" && $formData[setting.name] !== setting.default && "hidden") || (screen === "set" && $formData[setting.name] === setting.default && "hidden") : "block"}`}
+      >
+        <Form.Field
+          {form}
+          name={`${setting.name}`}
+          class="my-2 {$formData[setting.name] === setting.default &&
+            'opacity-50'}"
+        >
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label
+                >{setting.name}
+                <span class="text-gray-500"
+                  >(Default: {setting.default
+                    ? setting.default
+                    : "Empty"})</span
+                >
+              </Form.Label>
+              <div class="ms-2 space-y-2">
+                <Input {...props} bind:value={$formData[setting.name]} />
+              </div>
+            {/snippet}
+          </Form.Control>
+        </Form.Field>
+      </div>
+    {/if} -->
+    {#if setting.type === "string[]"}
+      <div
+        class={`${screen !== "all" ? (screen === "unset" && $formData[setting.name] !== setting.default && "hidden") || (screen === "set" && $formData[setting.name] === setting.default && "hidden") : "block"}`}
+      >
+        <Form.Field
+          {form}
+          name={`${setting.name}`}
+          class="my-2 {JSON.stringify($formData[setting.name]) ===
+            JSON.stringify(setting.default) && 'opacity-50'}"
+        >
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label
+                >{setting.name}
+                <span class="text-gray-500">(Default: {setting.default})</span>
+                <Button
+                  class="text-green-500 ${$formData[setting.name] ===
+                    setting.default && 'hidden disabled'}"
+                  variant="link"
+                  onclick={() => ($formData[setting.name] = setting.default)}
+                  >clear</Button
+                >
+              </Form.Label>
+              <ObjectInput {...props} bind:value={$formData[setting.name]} />
+            {/snippet}
+          </Form.Control>
+        </Form.Field>
+      </div>
+    {/if}
+    {#if setting.type === "object[]"}
+      <div
+        class={`${screen !== "all" ? (screen === "unset" && $formData[setting.name] !== setting.default && "hidden") || (screen === "set" && $formData[setting.name] === setting.default && "hidden") : "block"}`}
+      >
+        <Form.Field
+          {form}
+          name={`${setting.name}`}
+          class="my-2 {JSON.stringify($formData[setting.name]) ===
+            JSON.stringify(setting.default) && 'opacity-50'}"
+        >
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label
+                >{setting.name}
+                <span class="text-gray-500">(Default: {setting.default})</span>
+              </Form.Label>
+              <ObjectInput {...props} bind:value={$formData[setting.name]} />
+            {/snippet}
+          </Form.Control>
+        </Form.Field>
+      </div>
+    {/if}
+  {/each}
 
   <Form.Button aria-label="submit">Submit</Form.Button>
 </form>
