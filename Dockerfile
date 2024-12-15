5bbb45f7ffda17452a03052c481adac99ae627a8
skadefro <allan@openrpa.dk>
2024-12-15 15:46:49 +0100

    add docker file, fix env for docker deployment

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:18-alpine AS builder
+WORKDIR /app
+COPY package*.json .
+RUN npm ci
+COPY . .
+RUN npm run build
+RUN npm prune --production
+
+
+FROM node:18-alpine
+WORKDIR /app
+COPY --from=builder /app/build build/
+COPY --from=builder /app/node_modules node_modules/
+COPY package.json .
+EXPOSE 3000
+ENV NODE_ENV=production
+CMD [ "node", "build" ]
+
+# docker build -t cloudhack/core-web:0.0.7 . && docker run -it -p 80:3000 --rm cloudhack/core-web:0.0.7
+# docker build -t cloudhack/core-web:0.0.7 . && docker run -it -p 80:3000 --rm cloudhack/core-web:0.0.7
+# docker build -t cloudhack/core-web:0.0.7 . && docker push cloudhack/core-web:0.0.7
+
+# docker run -it -p 80:3000 --rm cloudhack/core-web:0.0.7
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,14 @@
 import type { Handle } from '@sveltejs/kit';
-import { domain, client_id } from '$env/static/private';
+import { web_domain, web_client_id } from '$env/static/private';
 import { auth } from '$lib/stores/auth.svelte';
 export const handle: Handle = async ({ event, resolve }) => {
     const { url, cookies } = event;
-    await auth.clientinit(domain, client_id, url.origin, fetch, cookies );
+    await auth.clientinit(web_domain, web_client_id, url.origin, fetch, cookies );
     event.locals = event.locals || {};
-    console. debug("hooks.server.ts:", "conn", auth.isConnected, "auth", auth.isAuthenticated, auth.profile?.name);
+    console. debug("hooks.server.ts:", web_domain, "conn", auth.isConnected, "auth", auth.isAuthenticated, auth.profile?.name);
     // @ts-ignore
-    event.locals.domain = domain;
+    event.locals.domain = web_domain;
     // @ts-ignore
-    event.locals.client_id = client_id;
+    event.locals.client_id = web_client_id;
     return await resolve(event);
 };
