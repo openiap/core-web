8efbf15985e94994d2b37db1c3d7b55d4e288b88
skadefro <allan@openrpa.dk>
2025-01-01 15:08:43 +0100

    fix types / fix edit user

--- src/routes/workspace/[id]/+page.ts
+++ src/routes/workspace/[id]/+page.ts
@@ -2,9 +2,9 @@ import { auth } from "$lib/stores/auth.svelte.js";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { editFormSchema } from "../schema.js";
-import type { PageServerLoad } from "./$types.js";
+import type { PageLoad } from "./$types.js";
 
-export const load: PageServerLoad = async ({ parent, params }) => {
+export const load: PageLoad = async ({ parent, params }) => {
   const { access_token } = await parent();
   try {
     let item = await auth.client.FindOne<any>({ collectionname: "users", query: { _id: params.id }, jwt: access_token });
