5ce1169d96053e43b206054e98ce6c4123a4100e
skadefro <allan@openrpa.dk>
2025-01-01 15:10:30 +0100

    fix role, package and mailhistory

---
+++ src/routes/mailhistory/[id]/+page.ts
@@ -0,0 +1,13 @@
+import { auth } from "$lib/stores/auth.svelte.js";
+import type { PageLoad } from "./$types.js";
+import { collectionname } from "./+page.svelte";
+export const load: PageLoad = async ({ parent, params }) => {
+  const { access_token } = await parent();
+  let id = ;
+  try {
+    let item = await auth.client.FindOne<any>({ collectionname, query: { _id: params.id }, jwt: access_token });
+    return { item };
+  } catch (error) {
+    return { item: null };    
+  }
+};
