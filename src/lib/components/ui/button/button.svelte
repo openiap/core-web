0a0390b309b438fdb9930a2fb81e8c02b46d1f6a
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-01-09 18:00:26 +0500

    updated buttons color

--- src/lib/components/ui/button/button.svelte
+++ src/lib/components/ui/button/button.svelte
@@ -11,7 +11,7 @@
 		variants: {
 			variant: {
 				default:
-					"bg-primary text-primary-foreground hover:bg-primary/90",
+					"bg-gray-400 text-primary-foreground hover:bg-primary/90",
 				destructive:
 					"bg-destructive text-destructive-foreground hover:bg-destructive/90",
 				outline:
