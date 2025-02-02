3807c3cefac704e4f3d1b9dd9d929cfb0651e444
skadefro <allan@openrpa.dk>
2025-02-02 10:32:04 +0100

    add debounc to searchinput

--- src/lib/searchinput/searchinput.svelte
+++ src/lib/searchinput/searchinput.svelte
@@ -2,7 +2,25 @@
 	import { HotkeyInput } from "$lib/components/ui/hotkeyinput";
 	import { Search } from "lucide-svelte";
 
-	let { searchstring = $bindable("") } = $props();
+	let { searchstring = $bindable(""), debounc = 500 } = $props();
+	let _searchstring = $state(searchstring);
+	let lastinput = new Date();
+	let timeout:any = null;
+	$effect(() => {
+		if (new Date().getTime() - lastinput.getTime() > debounc) {
+			if(timeout != null) clearTimeout(timeout);
+			searchstring = _searchstring;
+			lastinput = new Date();
+		} else {
+			if(timeout != null) clearTimeout(timeout);
+			lastinput = new Date();
+			timeout = setTimeout(() => {
+				searchstring = _searchstring;
+				timeout = null;
+				lastinput = new Date();
+			}, debounc);
+		}
+	});
 </script>
 
 <div class="flex w-full max-w-[288px] flex-col gap-1.5 mb-4">
@@ -11,7 +29,7 @@
 		<HotkeyInput
 			id="searchstring"
 			placeholder="Search string or JSON query"
-			bind:value={searchstring}
+			bind:value={_searchstring}
 			data-shortcut={"ctrl+f,meta+f"}
 			type="text"
 			class="w-full h-7 pl-8 py-2 rounded-[10px] border dark:border-bw600 dark:placeholder-bw500 dark:text-bw500 dark:bg-bw850 focus:outline-none"
