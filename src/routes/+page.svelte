75db56f7a438301b661a0b6e495e962084bbb748
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-03-14 11:47:13 +0500

    Added logos to client libraries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,11 +14,15 @@
   import pythonSvg from "$lib/images/svgs/python.svg";
   import rustSvg from "$lib/images/svgs/rust.svg";
   import vscodeSvg from "$lib/images/svgs/vscode.svg";
+  import javalightSvg from "$lib/images/svgs/java_lightmode.svg";
+  import rustlightSvg from "$lib/images/svgs/rust_lightmode.svg";
+  import browerSvg from "$lib/images/svgs/web.svg";
   import { auth } from "$lib/stores/auth.svelte";
+  import { mode } from "mode-watcher";
 
   let name = $derived(() => auth.profile?.name || "World");
   const { data } = $props();
-
+  console.log("$mode", $mode);
   // Helper function to map state to valid badge variant
   function getBadgeVariant(
     state: string,
@@ -38,6 +42,7 @@
     {
       title: "NodeJS",
       logo: jsSvg,
+      logolight: jsSvg,
       buttons: {
         example: "https://github.com/skadefro/nodetest",
         package: {
@@ -49,6 +54,7 @@
     {
       title: "Rust",
       logo: rustSvg,
+      logolight: rustlightSvg,
       buttons: {
         example: "https://github.com/openiap/rustapi/tree/main/crates/cli",
         package: {
@@ -60,6 +66,7 @@
     {
       title: "C/C++",
       logo: cSvg,
+      logolight: cSvg,
       buttons: {
         example: "https://github.com/skadefro/ctest",
         package: { title: "headers", link: "" },
@@ -68,6 +75,7 @@
     {
       title: "Python",
       logo: pythonSvg,
+      logolight: pythonSvg,
       buttons: {
         example: "https://github.com/skadefro/pythontest",
         package: {
@@ -79,6 +87,7 @@
     {
       title: ".net",
       logo: dotnetSvg,
+      logolight: dotnetSvg,
       buttons: {
         example: "https://github.com/skadefro/dotnettest",
         package: {
@@ -90,6 +99,7 @@
     {
       title: "Java",
       logo: javaSvg,
+      logolight: javalightSvg,
       buttons: {
         example: "https://github.com/skadefro/javatest",
         package: {
@@ -101,6 +111,7 @@
     {
       title: "Php",
       logo: phpSvg,
+      logolight: phpSvg,
       buttons: {
         example: "https://github.com/skadefro/phptest",
         package: {
@@ -109,11 +120,23 @@
         },
       },
     },
+    {
+      title: "Brower",
+      logo: browerSvg,
+      logolight: browerSvg,
+      buttons: {
+        example: "https://github.com/openiap/core-web",
+        package: {
+          title: "esm",
+          link: "https://www.npmjs.com/package/@openiap/jsapi",
+        },
+      },
+    },
   ];
   const cardRoot = "text-sm bg-bw200 dark:bg-bw900 rounded-[10px] p-5";
 </script>
 
-{#if !data.entities || data.entities.length === 0}
+{#if !data.entities || data.entities.length == 0}
   <div class="mb-5 font-bold text-lg">Need help?</div>
   <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 mb-10">
     <div class={cardRoot}>
@@ -165,23 +188,31 @@
       class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-11 rounded-[10px]"
     >
       {#each clientlibraries as item}
-        <div class="flex items-start space-x-4">
-          <div class="flex shrink-0">
-            <img src={item?.logo} alt="VS Code" class="h-6 w-6" />
+        <div class="flex flex-col items-start space-y-2">
+          <div class="flex shrink-0 items-center space-x-2">
+            {#if $mode != null || undefined}
+              <img
+                src={$mode == "light" ? item?.logolight : item?.logo}
+                alt={item?.title + " logo"}
+                class="h-6 w-6"
+              />
+            {/if}
+            <div>{item?.title}</div>
           </div>
           <div class="flex flex-col space-y-4">
-            <div>{item?.title}</div>
             <div
               class="flex flex-row space-x-2.5 space-y-0 items-center sm:space-x-0 sm:flex-col sm:space-y-2.5 lg:space-y-0 lg:flex-row lg:space-x-2.5"
             >
               {#if item?.buttons?.package?.link}
                 <HotkeyButton
+                  size="sm"
                   onclick={() =>
                     window.open(item?.buttons?.package?.link, "_blank")}
                   >{item?.buttons?.package?.title}</HotkeyButton
                 >
               {/if}
               <HotkeyButton
+                size="sm"
                 onclick={() => window.open(item?.buttons?.example, "_blank")}
                 >Example</HotkeyButton
               >
@@ -243,7 +274,6 @@
   </div>
 {:else}
   <div class="container mx-auto p-4 space-y-4">
-    <!-- <div class="font-bold">Pending Human Workflows</div> -->
     <div class="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
       {#each data.entities as task}
         <Card.Root class="hover:shadow-lg transition-shadow">
