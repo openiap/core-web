286939828553d7a91e2f99507b85678982f80392
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-03-17 15:57:18 +0500

    Updated onbording text

--- src/lib/sidebar/onboarding.ts
+++ src/lib/sidebar/onboarding.ts
@@ -3,6 +3,7 @@ import { base } from "$app/paths";
 import { driver, type DriveStep } from "driver.js";
 export const driverObj = driver({
     showProgress: true,
+    allowClose: true,
     stagePadding: 0,
     onDestroyed: () => {
         // @ts-ignore
@@ -12,15 +13,15 @@ export const driverObj = driver({
 export const baseTour: DriveStep[] = [
     {
         popover: {
-            title: 'OpenCore',
-            description: 'Welcome to OpenCore! This is a quick tour to help you get started. Use arrow keys or click the Next button to navigate.',
+            title: 'Welcome!',
+            description: 'Welcome to OpenCore! This is a quick tour to help you get an overview of the platform. Use arrow keys or click the Next button to start the tour.',
         },
     },
     {
         element: '.toursidebar',
         popover: {
-            title: 'OpenCore',
-            description: 'You can find most actions in the side menu, like managing agents, packages, and settings.',
+            title: 'The Side Menu',
+            description: 'This is a quick tour to help you get an overview of the platform. Use arrow keys or click the Next button to start the tour',
             onNextClick: () => {
                 goto(base + '/workitem');
                 driverObj.moveNext();
@@ -31,7 +32,7 @@ export const baseTour: DriveStep[] = [
         element: '.tourcontent',
         popover: {
             side: "over",
-            title: 'OpenCore',
+            title: 'Additional tours',
             description: 'Press ? on the keyboard or the top menu to start a guided tour for that part of the system.',
             onPrevClick: () => {
                 goto(base + '/');
@@ -47,8 +48,8 @@ export const baseTour: DriveStep[] = [
         element: '.tourcontent',
         popover: {
             side: "over",
-            title: 'OpenCore',
-            description: 'Many menu items also have a keyboard shortcut, for instance you can press "g" and then "e" to jump to the Entities page.',
+            title: 'Hotkeys',
+            description: 'Many menu items also have a keyboard shortcut, for instance you can press "g" and then "e" to jump to the Entities page. You can view all the hotkeys by holding down ‘shift’ while clicking ‘+’ ‘?’.',
             onPrevClick: () => {
                 goto(base + '/workitem');
                 driverObj.movePrevious();
@@ -61,7 +62,7 @@ export const agentTour: DriveStep[] = [
     {
         element: '.touragents',
         popover: {
-            title: 'Manage agents.',
+            title: 'Manage Agents',
             description: 'Click here to Manage agents. this is also where you can create cloud agents. Agents are used to run the code you publish to OpenCore.',
             onNextClick: () => {
                 goto(base + '/agent');
@@ -77,15 +78,15 @@ export const agentTour: DriveStep[] = [
         element: '.tourcontent',
         popover: {
             side: "over",
-            title: 'Manage agents.',
+            title: 'Manage Agents',
             description: 'Agents can run both externaly, on your own infrastructure, or in the cloud. You can create cloud agents by clicking the "Add Agent" button.',
         },
     },
     {
         element: '.touraddagent',
         popover: {
-            title: 'Manage agents.',
-            description: 'Agent images are pre-configured environments that you can use to run your code, like node.js, python, rust. You can also find images for popular frameworks like dotnet and powershell. We even have images that comes preloaded applications like the low-code workflow engine NodeRED.',
+            title: 'Manage Agents',
+            description: 'Agent images are pre-configured environments that you can use to run your code, like node.js, python, rust. You can also find images for popular frameworks like dotnet and powershell. We even have images that come with preloaded applications like the low-code workflow engine NodeRED.',
             onNextClick: () => {
                 goto(base + '/agent/new');
                 driverObj.moveNext();
@@ -96,8 +97,8 @@ export const agentTour: DriveStep[] = [
         element: '.tourcontent',
         popover: {
             side: "over",
-            title: 'Manage agents,',
-            description: 'When managing agents, you can see the status of each agent, schedule packages and view container logs. If resource broker has been enabled, you can also select a plan that matches the requirements for your agent, some agent may need more RAM or CPU power to run effeicently.',
+            title: 'Manage Agents',
+            description: 'When managing agents, you can see the status of each agent, schedule packages and container logs. If resource broker has been enabled, you can also select a plan that matches the requirements for your agent. Please note, some agents may need more RAM or CPU power to run efficiently.',
             onPrevClick: () => {
                 goto(base + '/agent');
                 driverObj.movePrevious();
