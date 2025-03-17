44293eeff78dabd2cf95310f7c07bd3060240b2e
Nabeel-Rizwan-Quadri <mr.nabeelrizwan@gmail.com>
2025-03-17 19:55:36 +0500

    Updated login page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -5,7 +5,7 @@
   import Separator from "$lib/components/ui/separator/separator.svelte";
   import { CustomInput } from "$lib/custominput";
   import GoogleSvg from "$lib/images/login/google_g.svg";
-  import LeftImage from "$lib/images/login/left_side_image.png";
+  import LeftImage from "$lib/images/login/test.svg";
   import { auth } from "$lib/stores/auth.svelte";
   import { capitalizeFirstLetter } from "../../helper";
 
@@ -42,14 +42,18 @@
   class="grid md:grid lg:grid-cols-3 h-screen overflow-auto items-center justify-center dark:bg-[#191B1E] lg:dark:bg-bw950"
 >
   <div class="h-screen overflow-hidden lg:col-span-2 hidden lg:block">
-    <img src={LeftImage} alt="background image"/>
+    <div class="flex flex-col justify-end items-center h-full w-full">
+      <img src={LeftImage} alt="background image" />
+    </div>
   </div>
   <div
     class="h-full m-10 lg:m-0 lg:col-span-1 lg:border-s-[1px] border-bw600 flex items-center dark:bg-[#191B1E]"
   >
     <Card.Root class="mx-auto max-w-sm border-hidden">
       <Card.Header>
-        <Card.Title class="dark:text-bw100 text-2xl mb-7">Login</Card.Title>
+        <Card.Title class="dark:text-bw100 text-2xl mb-7 font-normal"
+          >Login</Card.Title
+        >
         <Card.Description
           >Enter your email below to login to your account</Card.Description
         >
@@ -58,7 +62,7 @@
         <form method="post" action="{auth.baseurl}/local">
           <div class="grid gap-4">
             <div class="grid gap-2">
-              <Label class="dark:text-bw100" for="email">Email</Label>
+              <Label class="dark:text-bw100 font-normal" for="email">Email</Label>
               <CustomInput
                 class="dark:bg-transparent"
                 width=""
@@ -72,7 +76,7 @@
             </div>
             <div class="grid gap-2">
               <div class="flex items-center">
-                <Label class="dark:text-bw100" for="password">Password</Label>
+                <Label class="dark:text-bw100 font-normal" for="password">Password</Label>
                 <!-- <a href="##" class="ml-auto inline-block text-sm underline">
                     Forgot your password?
                   </a> -->
