432911b3b173dc8fb6d387e88f1fc489d829f915
skadefro <allan@openrpa.dk>
2025-03-18 08:55:35 +0100

    remove default value

--- src/routes/billingaccount/schema.ts
+++ src/routes/billingaccount/schema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 export const newCustomerSchema = z.object({
-  name: z.string().min(2).default("Contoso Corporation"),
+  name: z.string().min(2).default(""),
   _type: z.string().default("customer"),
 });
 export type NewCustomerSchema = typeof newCustomerSchema;
