38e549a4d21f286b71190e72ab8ffcf20e7bf632
skadefro <allan@openrpa.dk>
2025-03-26 19:30:06 +0100

    fix

--- src/routes/entities/[collection]/new/+page.svelte
+++ src/routes/entities/[collection]/new/+page.svelte
@@ -58,11 +58,6 @@
   });
   const { form: formData, enhance, message, validateForm } = form;
   try {
-    // if(auth.config.timeseries_collections.indexOf(data.collectionname)) {
-    //   formData.set({  });
-    // } else {
-    //   formData.set({ name: "new item", _type: "test" });
-    // }
     formData.set({ name: "new item", _type: "test" });
     validateForm({ update: true });
   } catch (error: any) {
