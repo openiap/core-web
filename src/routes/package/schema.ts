56f8fe404ec35e5794a40e2f151dc606ade809a6
skadefro <allan@openrpa.dk>
2025-03-29 09:29:15 +0100

    update newFormSchema to set default name to an empty string

--- src/routes/package/schema.ts
+++ src/routes/package/schema.ts
@@ -1,11 +1,12 @@
 import { z } from 'zod';
 
 export const newFormSchema = z.object({
-  name: z.string().min(2).default("Test "),
+  name: z.string().min(2).default(""),
   language: z.string().min(2).default("nodejs"),
   daemon: z.boolean(),
   chromium: z.boolean(),
   fileid: z.string().min(2),
+  // fileid: z.string().optional().default(""),
   _type: z.string().default("package"),
 });
 export type NewFormSchema = typeof newFormSchema;
