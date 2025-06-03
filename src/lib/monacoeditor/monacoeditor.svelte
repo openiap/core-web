2435c5722dc8cabbd535024489ae5b9e760988dd
nabeel-rizwan-quadri <mr.nabeelrizwan@gmail,com>
2025-06-03 17:38:43 +0200

    Added light and dark mode

--- src/lib/monacoeditor/monacoeditor.svelte
+++ src/lib/monacoeditor/monacoeditor.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { browser } from "$app/environment";
-  import { onMount, createEventDispatcher } from "svelte";
+  import { onMount, createEventDispatcher, onDestroy } from "svelte";
   const dispatch = createEventDispatcher();
-
   let Monaco: any;
-  let editorDiv:any;
+  let editorDiv: any;
   let model: any;
   let editor: any;
   let { code, language } = $props();
@@ -13,6 +12,12 @@
   import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker&inline";
   import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker&inline";
   import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker&inline";
+  import { mode } from "mode-watcher";
+  import { derived } from "svelte/store";
+
+  const currentTheme = derived(mode, (m) =>
+    m === "dark" ? "vs-dark" : "vs-light",
+  );
 
   async function init() {
     if (browser) {
@@ -36,6 +41,7 @@
         },
       };
       editor = Monaco.editor.create(editorDiv, {
+        theme: $currentTheme,
         value: code,
         language: language,
         automaticLayout: true,
@@ -45,9 +51,8 @@
       // dispatch code change events to parent
       editor.onDidChangeModelContent(() => {
         const value = model.getValue();
-        dispatch('change', { code: value });
+        dispatch("change", { code: value });
       });
-
 
       return () => {
         editor.dispose();
@@ -57,16 +62,28 @@
   onMount(() => {
     init();
   });
-  let fakeit = null;
-  let fakecode = null;
-  $effect(() => {
-    fakeit = language;  
-    fakecode = code;
-    if (browser && model && code) {
-      model.setValue(code);
-      Monaco.editor.setModelLanguage(model, language);
-    }
+  onDestroy(() => {
+    editor?.dispose();
   });
 
+  let fakeit = null;
+  let fakecode = null;
+
+  // react to theme or code/language changes
+  $effect(() => {
+    fakeit = language;
+    fakecode = code;
+    if (browser && Monaco) {
+      Monaco.editor.setTheme($currentTheme);
+      if (model) {
+        model.setValue(code);
+        Monaco.editor.setModelLanguage(model, language);
+      }
+    }
+  });
 </script>
-<div bind:this={editorDiv} class="h-full w-full"></div>
+
+<div
+  bind:this={editorDiv}
+  class="h-full w-full bg-bw200 dark:bg-bw850 border dark:border-bw600 rounded-[10px] overflow-hidden"
+></div>
