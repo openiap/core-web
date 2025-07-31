b7b9c69dfbe7c8caf6b802ea6b8d8c67ffa40272
nabeel-rizwan-quadri <mr.nabeelrizwan@gmail,com>
2025-07-31 16:54:14 +0200

    Add 'tag' and 'anonymous' fields to editFormSchema and remove 'always_parse_http'

--- src/routes/serverless/schema.ts
+++ src/routes/serverless/schema.ts
@@ -10,13 +10,15 @@ export type NewFormSchema = typeof newFormSchema;
 export const editFormSchema = z.object({
   min_instances: z.number().min(0).max(2).default(0),
   max_instances: z.number().min(1).max(3).default(1),
-  always_parse_http: z.boolean().optional().nullable().transform((val) => val ?? true),
   volume: z.string().optional().nullable().transform((val) => val ?? ""),
   distro: z.string().optional().nullable().transform((val) => val ?? ""),
   repo: z.string().optional().nullable().transform((val) => val ?? ""),
   name: z.string().optional().nullable().transform((val) => val ?? ""),
+  port: z.number().optional().nullable().transform((val) => val ?? 0),
   env: z.any(),
   minimum_response_time: z.number().min(0).default(0),
   runas: z.string().optional().nullable().transform((val) => val ?? ""),
+  tag: z.string().optional().nullable().transform((val) => val ?? ""),
+  anonymous: z.boolean().default(false),
 }).passthrough();
 export type EditFormSchema = typeof editFormSchema;
