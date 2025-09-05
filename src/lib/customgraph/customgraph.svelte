69ecfac05582040f2c464ea2b254215049602e0e
nabeel-rizwan-quadri <mr.nabeelrizwan@gmail,com>
2025-09-05 11:10:09 +0200

    removed logs

--- src/lib/customgraph/customgraph.svelte
+++ src/lib/customgraph/customgraph.svelte
@@ -333,8 +333,6 @@
                         },
                     };
 
-                    console.log("s.label", title, s.label);
-
                     const setStyle = (hex: any) => {
                         styleObj.stroke = hex;
                         styleObj.fill = `${hex}20`; // 8-digit hex with alpha
@@ -350,16 +348,13 @@
 
                     if (!Number.isNaN(code)) {
                         if (code >= 200 && code <= 250) {
-                            console.log("green", code);
                             setStyle($mode === "dark" ? "#2DD4BF" : "#10B981"); // 200–250 -> green
                         } else if (
                             (code >= 401 && code <= 403) ||
                             (code >= 500 && code <= 600)
                         ) {
-                            console.log("red", code);
                             setStyle($mode === "dark" ? "#F87171" : "#B91C1C"); // 401–403 and 500–600 -> red
                         } else {
-                            console.log("blue", code);
                             setStyle($mode === "dark" ? "#60A5FA" : "#3B82F6"); // everything else -> blue
                         }
                     }
